c004c9f52af86083ce5517a6ca43be7f8c8f335b
nurtai325 <140373832+nurtai325@users.noreply.github.com>
2024-05-28 18:12:16 +0500

    no changes

--- server/database/seeders/DatabaseSeeder.php
+++ server/database/seeders/DatabaseSeeder.php
@@ -16,8 +16,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Nurtai',
+            'email' => 'nurtolymbek23@gmail.com',
+            'password' => bcrypt('password'),
         ]);
     }
 }
