a287f38d447fd70828d489b54760667fb7298e8a
nurtai325 <140373832+nurtai325@users.noreply.github.com>
2024-06-10 21:39:59 +0500

    fixed

--- app/Http/Controllers/Booking/WhatsappController.php
+++ app/Http/Controllers/Booking/WhatsappController.php
@@ -8,29 +8,27 @@ use Illuminate\Support\Facades\Log;
 class WhatsappController
 {
     public function webhook(Request $request) {
-        $data = $this->getHubChallenge($request->fullUrl());
+        $url = $request->getQueryString();
+        $ch = "";
 
-        return $data;
-    }
+        $found = false;
+        foreach (str_split($url) as $i) {
+            if (!$found) {
+                if ($i === '=') {
+                    $found = true;
+                    continue;
+                }
+            } else {
+                if ($i === '&') {
+                    break;
+                }
+            }
 
-    function getHubChallenge($inputString)
-    {
-        // Parse the URL to get the query part
-        $urlComponents = parse_url($inputString);
-
-        // Check if query part exists
-        if (!isset($urlComponents['query'])) {
-            return "hub.challenge parameter is not found.";
+            if ($found) {
+                $ch .= $i;
+            }
         }
 
-        // Parse the query string into an associative array
-        parse_str($urlComponents['query'], $queryParams);
-
-        // Check if 'hub.challenge' parameter is set
-        if (isset($queryParams['hub.challenge'])) {
-            return $queryParams['hub.challenge'];
-        } else {
-            return "hub.challenge parameter is not found.";
-        }
+        return $ch;
     }
 }
