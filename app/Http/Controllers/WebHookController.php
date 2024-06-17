43166cf8e1a181e06b6bde69e69b2f6ee54c85aa
nurtai325 <140373832+nurtai325@users.noreply.github.com>
2024-06-17 22:30:16 +0500

    removed logs files from git

--- app/Http/Controllers/WebHookController.php
+++ app/Http/Controllers/WebHookController.php
@@ -7,5 +7,9 @@ use Illuminate\Http\Request;
 
 class WebHookController extends WebhookHandler
 {
-    
+    protected function handleChatMessage(\Stringable $text): void
+    {
+        // in this example, a received message is sent back to the chat
+        $this->chat->html("Received: $text")->send();
+    }
 }
