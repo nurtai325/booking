7d91dbc1702bc777970312be07ca819f794b5ac4
nurtai325 <140373832+nurtai325@users.noreply.github.com>
2024-06-18 17:27:24 +0500

    got my controller again

--- config/telegraph.php
+++ config/telegraph.php
@@ -29,7 +29,7 @@ return [
          *
          * For reference, see https://docs.defstudio.it/telegraph/webhooks/overview
          */
-        'handler' => \DefStudio\Telegraph\Handlers\EmptyWebhookHandler::class,
+        'handler' => WebHookController::class,
 
         /*
          * Middleware to be applied to the webhook route
