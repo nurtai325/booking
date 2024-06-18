033b0a43bc3ad7e508847ae63887c4e735b6bd80
nurtai325 <140373832+nurtai325@users.noreply.github.com>
2024-06-18 20:43:24 +0500

    added advanced logging

--- app/Booking/Messaging/ChatGPT.php
+++ app/Booking/Messaging/ChatGPT.php
@@ -22,14 +22,16 @@ class ChatGPT
         $open_ai_key = getenv('OPENAI_API_KEY');
         $open_ai = new OpenAi($open_ai_key);
 
+        $messages = $this->getMessages($chat_id, $token);
         $chat = $open_ai->chat([
             'model' => 'gpt-4o',
-            'messages' => $this->getMessages($chat_id, $token),
+            'messages' => $messages,
             'temperature' => 1.0,
             'max_tokens' => 150,
             'frequency_penalty' => 0,
             'presence_penalty' => 0,
         ]);
+        var_dump($messages);
 
         $d = json_decode($chat);
         $content = $d->choices[0]->message->content;
