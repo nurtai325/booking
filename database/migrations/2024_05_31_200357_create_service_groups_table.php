9dbb0e292c080b335c1d822f4b01fa7d2d95a5d7
nurtai325 <140373832+nurtai325@users.noreply.github.com>
2024-06-21 11:12:07 +0500

    modified schema

--- database/migrations/2024_05_31_200357_create_service_groups_table.php
+++ database/migrations/2024_05_31_200357_create_service_groups_table.php
@@ -5,6 +5,7 @@ namespace server\database\migrations;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use PHPUnit\TextUI\XmlConfiguration\SchemaDetectionResult;
 
 return new class extends Migration {
     /**
@@ -12,38 +13,57 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        Schema::create("services", function (Blueprint $table) {
-            $table->id("service_id");
+        Schema::create("service_groups", function (Blueprint $table) {
+            $table->id();
             $table->unsignedBigInteger('user_id');
             $table->string("name")->unique();
             $table->string("description");
-            $table->float("price");
-            $table->integer("capacity");
             $table->string("additional_info")->nullable();
-            $table->integer('duration');
 
             $table->foreign("user_id")->references("id")->on("users");
         });
 
+        Schema::create("services", function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('service_group');
+            $table->string("name")->unique();
+            $table->string("description");
+            $table->float("price");
+            $table->integer("quantity");
+            $table->string("additional_info")->nullable();
+            $table->integer('duration');
+
+            $table->foreign("service_group")->references("id")->on("service_group");
+        });
+
         Schema::create("bookings", function (Blueprint $table) {
-            $table->id("booking_id");
+            $table->id();
             $table->unsignedBigInteger("service_id");
             $table->time("start_time");
+            $table->time("end_time");
+            $table->integer("booked");
 
-            $table->foreign("service_id")->references("service_id")->on("services");
+            $table->foreign("service_id")->references("id")->on("services");
         });
 
         Schema::create("records", function (Blueprint $table) {
-            $table->id("record_id");
+            $table->id();
             $table->string("name");
             $table->string("phone");
             $table->string("additional_info")->nullable();
-            $table->unsignedBigInteger("booking_id");
-            $table->boolean("canceled");
             $table->timestamps();
+            $table->integer("quantity");
 
-            $table->foreign("booking_id")
-                ->references("booking_id")->on("bookings");
+            $table->foreign("booking_id")->references("id")->on("bookings");
+        });
+
+        Schema::create("record_bookings", function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("record_id");
+            $table->unsignedBigInteger("booking_id");
+
+            $table->foreign("booking_id")->references("id")->on("bookings");
+            $table->foreign("record_id")->references("id")->on("records");
         });
     }
 
@@ -52,5 +72,9 @@ return new class extends Migration {
      */
     public function down(): void
     {
+        Schema::dropIfExists('service_groups');
+        Schema::dropIfExists('service');
+        Schema::dropIfExists('records');
+        Schema::dropIfExists('record_bookings');
     }
 };
